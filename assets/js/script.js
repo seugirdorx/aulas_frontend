80bcd18f862c24486bdca266772ff31ec69fa063
seugirdorx <amandagetion@gmail.com>
2022-11-17 16:39:39 -0300

    SiteComJS_1711

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+let nome = window.document.getElementById('nome')
+
+let email = document.querySelector('#email')
+
+let assunto = document.querySelector('#assunto')
+
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+nome.style.width = '36%'
+email.style.width = '36%'
+
+function validarNome(){
+    let txtNome = document.querySelector('#txtNome')
+
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome inválido'
+        txtNome.style.color = 'red'
+        nomeOk = false
+    }
+    else{
+        txtNome.innerHTML = 'Nome válido'
+        txtNome.style.color = 'rgb(155, 235, 8)'
+        nomeOk = true
+    }
+}
+
+function validarEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 || email.value.indexOf('com') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+        emailOk = false
+    }
+    else{
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'rgb(155, 235, 8)'
+        emailOk = true
+    }
+}
+
+function validarAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if(assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+        assuntoOk = false
+    }
+    else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar(){
+    
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert ("Formulário enviado com sucesso!")
+    }
+    else{
+        alert ("Preencha o formulário corretamente antes de enviar!")
+    }
+}
